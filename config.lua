688de14ca7e9243045c209270d17b98fa6283105
DenisOPementinha <51972039+DenisOPementinha@users.noreply.github.com>
2019-06-19 17:13:02 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,6 @@ Config = {}
 Config.Locale = 'br'
 
 Config.EnableAntiSpam = true -- Ativa ou desativa o antispam do sistema.
-Config.WaitingTime = 20s
+Config.WaitingTime = 20
 Config.CommunityLink = 'COMMUNITY_LINK' --Mude para o link do seu discord ou website
 Config.PlayersToStartRocade = 32 --Suporte ate 255 slots
